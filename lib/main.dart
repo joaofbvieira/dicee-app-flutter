81d320656628944a47be1eb68ba2ed151963002c
joaofbvieira <joao.fbvieira@gmail.com>
2020-03-27 17:30:53 -0300

    Random generating dices individually and all dices

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +17,88 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  final leftDiceKeyState = GlobalKey<_DiceButtonState>();
+  final rightDiceKeyState = GlobalKey<_DiceButtonState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: DiceButton(
+                key: leftDiceKeyState,
+              ),
+            ),
+            Expanded(
+              child: DiceButton(
+                key: rightDiceKeyState,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 50.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceKeyState.currentState.setRandomDiceNumber();
+                  rightDiceKeyState.currentState.setRandomDiceNumber();
+                });
+              },
+              child: Icon(Icons.play_arrow),
+              backgroundColor: Colors.black,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class DiceButton extends StatefulWidget {
+  final int initialDiceNumber;
+
+  const DiceButton({Key key, this.initialDiceNumber = 1}) : super(key: key);
+
+  @override
+  _DiceButtonState createState() => _DiceButtonState();
+}
+
+class _DiceButtonState extends State<DiceButton> {
+  int diceNumber;
+
+  @override
+  void initState() {
+    super.initState();
+    diceNumber = widget.initialDiceNumber;
+  }
+
+  void setRandomDiceNumber() {
+    diceNumber = 1 + Random().nextInt(6);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        setState(() {
+          setRandomDiceNumber();
+        });
+      },
+      child: Image.asset('images/dice$diceNumber.png'),
+    );
   }
 }
